eb0f4290a5844f79727b7ffa087bc56a422f4140
Barry Moy <bmoy1@yahoo.com>
2018-07-23 19:40:25 -0500

    clean up code to make linter happy

--- project1/authentication.py
+++ project1/authentication.py
@@ -2,14 +2,12 @@
 """This module provides functions for authenticating users."""
 
 def login(username, password):
+    """Log the user in."""
     try:
         user_file = open('/etc/users.txt')
         user_buf = user_file.read()
         users = [line.split("|") for line in user_buf.split("\n")]
-        if [username, password] in users:
-            return True
-        else:
-            return False
-    except IOError as e:
+        return [username, password] in users
+    except IOError:
         print("I can't authenticate you.")
         return False
